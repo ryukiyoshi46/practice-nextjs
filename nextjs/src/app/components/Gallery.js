9d4c4c6d5e75e445d37a93b080a3e98f1f73b9b1
ryukiyoshi46 <reina.yukiyoshi@actbe.co.jp>
2024-05-07 19:50:23 +0900

    トップ：gallery.json修正に伴いスライダー修正

--- nextjs/src/app/components/Gallery.js
+++ nextjs/src/app/components/Gallery.js
@@ -17,16 +17,21 @@ export const Gallery = () => {
   const [currentCategory, setCurrentCategory] = useState('category01');
   const [galleryItems, setGalleryItems] = useState([]);
 
-  // カテゴリーの切り替え
-  useEffect(() => {
-    if(galleryData.categories[currentCategory]) {
-      setGalleryItems(galleryData.categories[currentCategory])
-    }
-  }, [currentCategory])
+  // カテゴリーに対応するデータを取得する関数
+  const getGalleryDataByCategory = (categoryName) => {
+    const categoryData = galleryData.categories.find(category => category.categoryName === categoryName);
+    return categoryData ? categoryData.data : [];
+  };
 
+  // カテゴリーが変更されたらデータを取得する
   const handleCategoryClick = (category) => {
     setCurrentCategory(category);
   };
+
+  useEffect(() => {
+    const data = getGalleryDataByCategory(currentCategory);
+    setGalleryItems(data);
+  }, [currentCategory]);
 
   // Swiper設定
   const slideSettings = {
@@ -45,46 +50,54 @@ export const Gallery = () => {
 
       <div className="l-gallery__category">
         <ul className="l-gallery__categoryList">
-          <li className={`l-gallery__categoryItem ${currentCategory === 'category01' ? 'is-current' : ''}`} onClick={() => handleCategoryClick('category01')}>観測装置１</li>
-          <li className={`l-gallery__categoryItem ${currentCategory === 'category02' ? 'is-current' : ''}`} onClick={() => handleCategoryClick('category02')}>91㎝反射望遠鏡</li>
-          <li className={`l-gallery__categoryItem ${currentCategory === 'category03' ? 'is-current' : ''}`} onClick={() => handleCategoryClick('category03')}>観測装置２</li>
+          {galleryData.categories.map((categoryItem) => (
+            <li
+              key={categoryItem.categoryName}
+              className={`l-gallery__categoryItem ${currentCategory === categoryItem.categoryName ? 'is-current' : ''}`}
+              onClick={() => handleCategoryClick(categoryItem.categoryName)}
+            >
+              {categoryItem.categoryLabel}
+            </li>
+          ))}
         </ul>
       </div>
 
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay, EffectCoverflow]}
-        breakpoints={slideSettings}
-        slidesPerView={"auto"}
-        centeredSlides={true}
-        direction={"horizontal"}
-        loop
-        speed={1000}
-        navigation
-        initialSlide="1"
-        effect={'coverflow'}
-        coverflowEffect={{
-          rotate: 100,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        className="l-gallery__slider"
-      >
-        {galleryItems.map((galleryItem, index) => (
-          <SwiperSlide key={index} className="l-gallery__sliderItem">
-            <div className="l-gallery__sliderWrap">
-              <img src={galleryItem.imgPath01} alt="スライダー画像" className="l-gallery__sliderImg" />
-              <img src="/img_slider_shadow01.png" alt="" className="l-gallery__sliderShadow" />
-            </div>
-            <h3 className="l-gallery__sliderName">{galleryItem.name}</h3>
-            <Button hrefURL={`/${galleryItem.id}`} addClasses={"l-gallery__sliderButton"}>詳細を見る</Button>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {galleryItems.length > 0 && (
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay, EffectCoverflow]}
+          breakpoints={slideSettings}
+          slidesPerView={"auto"}
+          centeredSlides={true}
+          direction={"horizontal"}
+          loop
+          speed={1000}
+          navigation
+          initialSlide="1"
+          effect={'coverflow'}
+          coverflowEffect={{
+            rotate: 100,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          className="l-gallery__slider"
+        >
+          {galleryItems.map((galleryItem, index) => (
+            <SwiperSlide key={index} className="l-gallery__sliderItem">
+              <div className="l-gallery__sliderWrap">
+                <img src={galleryItem.imgPath01} alt="スライダー画像" className="l-gallery__sliderImg" />
+                <img src="/img_slider_shadow01.png" alt="" className="l-gallery__sliderShadow" />
+              </div>
+              <h3 className="l-gallery__sliderName">{galleryItem.name}</h3>
+              <Button hrefURL={`/${galleryItem.id}`} addClasses={"l-gallery__sliderButton"}>詳細を見る</Button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   )
 }
