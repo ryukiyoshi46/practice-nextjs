0e7e8dbd172ad72ecc3437a4ce71d8c9f36f42b9
ryukiyoshi46 <reina.yukiyoshi@actbe.co.jp>
2024-05-07 21:04:52 +0900

    詳細：IDに紐づく詳細データ表示、クリックした画像をメインに表示させる

--- nextjs/src/app/[id]/page.js
+++ nextjs/src/app/[id]/page.js
@@ -6,9 +6,35 @@ import { Header } from "../components/Header";
 import galleryData from "../data/gallery"
 import styles from "../style.scss";
 import { Button } from "@/app/components/Button";
+import { usePathname } from 'next/navigation';
+import { useState } from "react";
 
 export default function DetailPage () {
-  console.log(galleryData)
+
+  // 現在のページのID取得
+  const pathname = usePathname()
+  const currentPageId = pathname.replace('/', '')
+
+  // IDに紐づくデータを取得
+  let currentPageData = null
+  let currentPageCategory = null
+
+  galleryData.categories.forEach(category => {
+    const dataItem = category.data.find(dataItem => currentPageId === String(dataItem.id))
+    if (dataItem) {
+      currentPageData = dataItem
+      currentPageCategory = category.categoryLabel
+      return
+    }
+  })
+
+  const [mainImage, setMainImage] = useState(currentPageData.imgPath01)
+  const handleChangeMainImage = (e) => {
+    console.log(e);
+    console.log(e.target.src);
+    setMainImage(e.target.src)
+    console.log('click')
+  }
 
   return (
     <>
@@ -18,21 +44,29 @@ export default function DetailPage () {
         <section className="p-detailPageContents">
           <div className="p-detailPageContents__inner">
             <div className="p-detailPageContents__img">
-              <img src="/img_dummy01.png" alt="メイン画像" className="p-detailPageContents__imgMain" />
+              <img src={mainImage} alt="メイン画像" className="p-detailPageContents__imgMain" />
               <ul className="p-detailPageContents__imgList">
-                <li className="p-detailPageContents__imgItem"><img src="/img_dummy01.png" alt="メイン画像" className="" /></li>
-                <li className="p-detailPageContents__imgItem"><img src="/img_dummy01.png" alt="メイン画像" className="" /></li>
-                <li className="p-detailPageContents__imgItem"><img src="/img_dummy01.png" alt="メイン画像" className="" /></li>
-                <li className="p-detailPageContents__imgItem"><img src="/img_dummy01.png" alt="メイン画像" className="" /></li>
+                { currentPageData.imgPath01 && (
+                  <li className="p-detailPageContents__imgItem"><img src={`${currentPageData.imgPath01}`} alt="メイン画像" className="" onClick={handleChangeMainImage} /></li>
+                )}
+                { currentPageData.imgPath02 && (
+                  <li className="p-detailPageContents__imgItem"><img src={`${currentPageData.imgPath02}`} alt="メイン画像" className="" onClick={handleChangeMainImage} /></li>
+                )}
+                { currentPageData.imgPath03 && (
+                  <li className="p-detailPageContents__imgItem"><img src={`${currentPageData.imgPath03}`} alt="メイン画像" className="" onClick={handleChangeMainImage} /></li>
+                )}
+                { currentPageData.imgPath04 && (
+                  <li className="p-detailPageContents__imgItem"><img src={`${currentPageData.imgPath04}`} alt="メイン画像" className="" onClick={handleChangeMainImage} /></li>
+                )}
+                { currentPageData.imgPath05 && (
+                  <li className="p-detailPageContents__imgItem"><img src={`${currentPageData.imgPath05}`} alt="メイン画像" className="" onClick={handleChangeMainImage} /></li>
+                )}
               </ul>
             </div>
             <div className="p-detailPageContents__explain">
-              <p className="p-detailPageContents__explainCategory">観測装置1</p>
-              <h1 className="p-detailPageContents__explainName">91cm反射望遠鏡</h1>
-              <p className="p-detailPageContents__explainText">
-                仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります<br />
-                仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります仮のテキストが入ります
-              </p>
+              <p className="p-detailPageContents__explainCategory">{currentPageCategory}</p>
+              <h1 className="p-detailPageContents__explainName">{currentPageData.name}</h1>
+              <p className="p-detailPageContents__explainText">{currentPageData.explanation}</p>
             </div>
           </div>
         </section>
